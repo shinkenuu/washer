9393bc3e01c480e76f6c6979c9b58daa30a0a94f
ndelatorre <natanael.delatorre@concrete.com.br>
2018-06-06 14:46:06 -0300

    refactors Schema to DRY and makes it iterable (to dict())

--- schemas.py
+++ schemas.py
@@ -1,20 +1,51 @@
 from datetime import datetime
+import json
 
-class CredentialSchema(object):
+
+class Schema(object):
+    attributes = []
+
     def __init__(self, **kwargs):
-        self.username = kwargs['username']
-        self.password =  kwargs['password']
-        self.able_to_vote = kwargs['able_to_vote']
+        for attr in self.attributes:
+            self.__setattr__(attr, kwargs[attr])
+
+    def __iter__(self):
+        """
+        Create a formatted ```self``` to a dict with only the attributes of the Schema
+        """
+        for attr in self.attributes:
+            key = attr
+            value = self.__getattribute__(attr)
+
+            if isinstance(value, Schema):
+                yield key, dict(value)
+                continue
+
+            yield key, value
+
+
+class CredentialSchema(Schema):
+    attributes = ['username', 'password', 'able_to_vote', 'last_vote_datetime']
+
+    def __init__(self, **kwargs):
+        super(Schema, self).__init__()
+
         self.last_vote_datetime = datetime.strptime(kwargs['last_vote_datetime'], '%Y-%m-%d %H:%M:%S')
 
 
-class ServerSchema(object):
+class ServerSchema(Schema):
+    attributes = ['name', 'base_url', 'credentials']
+
     def __init__(self, **kwargs):
-        self.name = kwargs['name']
-        self.base_url = kwargs['base_url']
+        super(Schema, self).__init__()
+
         self.credentials = [CredentialSchema(**credential_dict) for credential_dict in kwargs['credentials']]
 
 
-class WasherSchema(object):
+class WasherSchema(Schema):
+    attributes = ['servers']
+
     def __init__(self, **kwargs):
+        super(Schema, self).__init__()
+
         self.servers = [ServerSchema(**server_dict) for server_dict in kwargs['servers']]
